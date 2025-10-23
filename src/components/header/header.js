00de400cf625fc7bf8649d26e14142fb562b1e68
alorloff <alex.orlov.unofficial@gmail.com>
2025-10-23 17:09:08 +0300

    Signed-off-by: alorloff <alex.orlov.unofficial@gmail.com>

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import './header.scss'
+
+const Header = () => {
+    return (
+        <div className='header'> 
+            <div className='header__button'>
+                <span className='header__button__title'>ellorlove's</span>
+                <span>custom components</span>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/main/main.js
@@ -0,0 +1,9 @@
+import './main.scss'
+
+const Main = () => {
+    return (
+        <div className="main"></div>
+    )
+}
+
+export default Main;
